36b36b3ec94fdc1e7f038de62a784268e891538b
Nikolai Kirnosoff <kalun62@gmail.com>
2023-09-08 11:54:27 +0300

    Добавил popup

--- clickOnTags.js
+++ clickOnTags.js
@@ -2,12 +2,16 @@
 const linkApp = `https://script.google.com/macros/s/AKfycbxkQkFFvlRylt4iu57ZYXqbvEr5oQVOdpLTh3OtEVSqOulGzyYOuVGexnduDaXK_-sB/exec`
 
 const buttons = document.querySelectorAll('.btn')
+const popup = document.querySelector('.popup')
+const closed = document.querySelector('.closed')
 
 buttons.forEach(btn => {
 	btn. addEventListener('click', () => post(btn))
 })
 
-
+closed.addEventListener('click', () => {
+	window. close()
+})
 
 function post(item) {
 	const formData = new FormData()
@@ -15,5 +19,9 @@ function post(item) {
 	formData.append('id', item.getAttribute('id'))
 			
 	axios.post(linkApp, formData)
+
+	setTimeout(() => {
+		popup.style.display = 'flex'
+	},1000)
 
 }
