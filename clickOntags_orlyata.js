d576632fbc3adb0d5691088c63e8a64ae34f871f
Nikolai Kirnosoff <kalun62@gmail.com>
2023-11-01 20:53:19 +0300

    добавил файлы для орлят россии

---
+++ clickOntags_orlyata.js
@@ -0,0 +1,33 @@
+
+const linkApp = `https://script.google.com/macros/s/AKfycbxkQkFFvlRylt4iu57ZYXqbvEr5oQVOdpLTh3OtEVSqOulGzyYOuVGexnduDaXK_-sB/exec`
+
+const buttons = document.querySelectorAll('.btn')
+const popup = document.querySelector('.popup')
+const closed = document.querySelector('.closed')
+
+buttons.forEach(btn => {
+	btn.addEventListener('click', () => post(btn))
+})
+
+closed.addEventListener('click', () => {
+	window.location = `https://xn--80aaailatklfebdtdfov7aahtbt5b2bi9i1a.xn--p1ai/`
+})
+
+function post(item) {
+	const formData = new FormData()
+	if(!item.classList.contains('submit')){
+		formData.append('name', item.innerText.toLowerCase())
+	}else{
+		formData.append('name', item.previousElementSibling.value.toLowerCase())
+	}
+			
+	axios.post(linkApp, formData)
+
+	setTimeout(() => {
+		popup.style.display = 'flex'
+	},1000)
+
+}
+
+
+
